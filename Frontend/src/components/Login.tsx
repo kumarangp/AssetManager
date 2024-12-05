49de6b38f43f8bcf33b6728de59f8de4920bee52
kumarangp <kumaravelgp@gmail.com>
2024-12-05 13:35:26 +0530

    Created : Login Page

---
+++ Frontend/src/components/Login.tsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { Button, Checkbox, Form, Input, Typography } from "antd";
+
+const { Title, Link } = Typography;
+
+interface LoginFormValues {
+  email: string;
+  password: string;
+  remember: boolean;
+}
+
+
+const Login: React.FC = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleSubmit = async (values: LoginFormValues) => {
+    setLoading(true);
+    try {
+      console.log("Form values: ", values);
+      // call the API for login
+      // Ex: await loginUser(values)
+    } catch (error) {
+      console.error("Login failed: ", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div style={styles.container}>
+      <Title level={3}>
+        Login to Asset Manager
+      </Title>
+
+      <Form
+        name="login-form"
+        layout="vertical"
+        onFinish={handleSubmit}
+        style={styles.form}
+        initialValues={{ remember: true }}
+      >
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            { required: true, message: "Please input your email!" },
+            { type: "email", message: "Please enter a valid email!" }
+          ]}
+        >
+          <Input placeholder="Enter your email" />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password placeholder="Enter your password">
+          </Input.Password>
+        </Form.Item>
+        <Form.Item name="remember" valuePropName="checked">
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" loading={loading} block>
+            Login</Button>
+        </Form.Item>
+      </Form>
+      <Link style={styles.forgotPassword} onClick={() => console.log("Navigate to Forgot Password")}>
+        Forgot Password?
+      </Link>
+    </div>
+
+
+  );
+};
+
+const styles: React.CSSProperties = {
+  container: {
+    maxWidth: "400px",
+    margin: "150px auto 0",
+    padding: "20px",
+    textAlign: "center",
+    border: "1px solid #f0f0f0",
+    borderRadius: "8px",
+    backgroundColor: "#d5d5d5",
+    boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+  },
+  form: { marginTop: "20px" },
+  forgotPassword: { marginTop: "10px", display: "block", cursor: "pointer" },
+};
+
+export default Login;
+
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # AssetManager
+
+
+Commands Used
+
+git pull --rebase origin main
+git push origin main
+
+
+Create and checkout to new branch
+git checkout -b development
