0fc165d8ef2a3e5b7e377811be0eb2bb380e42af
kumarangp <kumaravelgp@gmail.com>
2024-12-06 16:37:51 +0530

    working dashboard without css

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,33 +1,21 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/dashboard/Dashboard";
-import Sidebar from "./components/dashboard/Sidebar";
-
 import Location from "./components/master/Location";
-
-
-import './App.css'
 
 const App: React.FC = () => {
   return (
-    <Dashboard />
-    // <Router>
-    //   <div style={{display: "flex"}}>
-    //     {/* Sidebar */}
-    //     <div style={{width: "200px"}}>
-    //       <Sidebar />
-    //     </div>
-
-    //     {/* Main Content */}
-    //     <div style={{flex: 1, padding: "20px"}}>
-    //       <Routes>
-    //         <Route path="/dashboard" element={<Dashboard />} />
-    //         <Route path="/master/location" element={<Location />} />
-    //       </Routes>
-    //     </div>
-    //   </div>
-    // </Router>
+    <Router>
+      <Routes>
+        {/* Dashboard Layout */}
+        <Route path="/" element={<Dashboard />}>
+          {/* Nested Routes */}
+          <Route path="dashboard" element={<div>Welcome to Dashboard!</div>} />
+          <Route path="settings/location" element={<Location />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 };
 
-export default App
+export default App;
--- Frontend/src/components/dashboard/Sidebar.tsx
+++ Frontend/src/components/dashboard/Sidebar.tsx
@@ -8,13 +8,13 @@ const Sidebar: React.FC = () => {
 
   const handleMenuClick = (menuKey: string) => {
     navigate(menuKey);
-  }
+  };
 
-  return(
-    <Menu 
+  return (
+    <Menu
       mode="inline"
       defaultSelectedKeys={["/dashboard"]}
-      style={{height: "100%"}}
+      style={{ height: "100%" }}
       onClick={(e) => handleMenuClick(e.key)}
     >
       <Menu.Item key="/dashboard" icon={<SettingOutlined />}>
@@ -22,7 +22,7 @@ const Sidebar: React.FC = () => {
       </Menu.Item>
 
       <Menu.SubMenu key="settings" icon={<SettingOutlined />} title="Settings">
-        <Menu.Item key="/master/location" icon={<EnvironmentOutlined />}>
+        <Menu.Item key="/settings/location" icon={<EnvironmentOutlined />}>
           Location
         </Menu.Item>
       </Menu.SubMenu>
